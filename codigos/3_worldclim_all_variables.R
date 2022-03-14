c1a182cc8ba69eb30fa2e8679c62ac5171ecc774
Santiago David <sdrrock@gmail.com>
2022-03-14 09:08:10 -0700

    fix code to create points using SpatialPoints

--- codigos/3_worldclim_all_variables.R
+++ codigos/3_worldclim_all_variables.R
@@ -1,21 +1,23 @@
 ## 03-Mar-22
-## extraer todas las 19 variables de Worldclim para las coordenadas
-## tratar de encontrar una forma de juntar localidades de acuerdo a su similaridad ambiental
+## extraer todas las 19 variables de Worldclim para las coordenadas unicas en la base de datos
 
-# load packages in script_1
+# cargar paquetes
+suppressPackageStartupMessages(library(tidyverse))
+library(readxl)
+library(raster)
 
-#load data
+# cargar datos
 field <- read_csv("sub_datos/paramo_field.csv")
 
 # extraer puntos unicos de gps para este subset de datos y algunas variables de localidad
 coordinates <- field %>% 
   distinct(decimalLongitude, decimalLatitude, Elevation, ParamoComplex, Department, Locality)
 
-# crear informacion espacial para las 483 coordenadas combinadas
-points <- SpatialPoints(coordinates[1:3], proj4string = r@crs)
-
-# define worldclim data
+# definir datos de worldclim para la cuadricula de Colombia
 r <- getData("worldclim", var = "bio", res = 0.5, lon = -77, lat = 1)
+
+# crear informacion espacial para las 483 coordenadas combinadas
+points <- SpatialPoints(coordinates[,c("decimalLongitude","decimalLatitude")], proj4string = r@crs)
 
 # extraer todas las 19 variables de WC, y unirlas a las coordenadas
 clim <- extract(r, points)
@@ -23,3 +25,7 @@ climate <- cbind.data.frame(coordinates, clim)
 
 # exportar
 write_csv(climate, "sub_datos/coordinates_wc_19variables.csv")
+
+
+
+
