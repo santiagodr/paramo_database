12d5f25ecd3b264a8021e184f13f831711e43109
Santiago David <sdrrock@gmail.com>
2022-04-06 10:54:35 -0700

    actualizar codigo 4 multivariate y exportar archivo con clusters y id's unicos para las localidades

--- codigos/4_multivariate.R
+++ codigos/4_multivariate.R
@@ -103,3 +103,102 @@ ggplot(envdata_clusters, aes(bio1_23, bio12_23, color = clusters)) +
 # NOTA: se deben revisar diferentes resultados tanto del PCA como del HC para entender
 # que variables dominan los componentes y la asignacion de clusters, el cluster 5 por ejemplo, 
 # no es tan definido y se sobrelapa con el 4 y el 3.
+
+
+# 31_Marzo_2022
+# continuacion de exploracion de resultados del analisis de cluster 
+
+# clusters por complejo de paramo
+envdata_clusters %>%
+  distinct(decimalLatitude, decimalLongitude, .keep_all = TRUE) %>% 
+  group_by(ParamoComplex, clusters) %>%
+  summarise(n()) %>% 
+  pivot_wider(names_from = clusters, values_from = `n()`)
+
+#la mayoria de complejos tienen localidades en dos clusters, pero dos complejos
+# tienen puntos hasta en 4 clusters: la cocha patascoy y chili barragan
+
+# Complejo de Paramos Chili-Barragan (CBG)
+library(leaflet)
+envdata_clusters %>%
+  filter(ParamoComplex == "Complejo de Paramos Chili-Barragan (CBG)") %>% 
+  distinct(decimalLongitude, decimalLatitude, Elevation) %>% 
+  leaflet() %>% 
+  addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
+  addCircleMarkers(~decimalLongitude, ~decimalLatitude)
+
+# Este complejo tiene puntos muy alejados en diferentes departamentos, por lo tanto 
+# es posible que la alta variación climatica se deba a la separacion geografica
+
+
+#### PARTE 3 ####
+# extraer informacion del PCA usando funciones de "FactoMineR"
+# para mas informacion explorar el link
+# http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/
+
+# extraer diferentes parametros
+eigen <- get_eigenvalue(res.pca)
+var <- get_pca_var(res.pca)
+var$coord
+var$cos2
+var$contrib
+
+# visualizar diferentes parametros
+fviz_pca_var(res.pca, col.var = "black")
+library("corrplot")
+corrplot(var$cos2, is.corr=FALSE)
+
+fviz_pca_var(res.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+             repel = TRUE # Avoid text overlapping
+)
+
+#contribucion de las variables en los primeros 2-3 componentes
+corrplot(var$contrib, is.corr=FALSE)  
+
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
+fviz_contrib(res.pca, choice = "var", axes = 1:2, top = 15)
+
+fviz_pca_var(res.pca, col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+)
+
+# Este codigo se puede usar si se desean cambiar los nombres de las variables de worldclim
+envdata %>% 
+  rename(Annual_Mean_Temp = bio1_23,
+         Mean_Diurnal_Range = bio2_23,
+         Isothermality = bio3_23,
+         Temp_seasonality = bio4_23,
+         Max_Temp_warmest_month = bio5_23,
+         Min_Temp_coldest_month = bio6_23,
+         Temp_annual_range = bio7_23,
+         Mean_Temp_wettest_quarter = bio8_23,
+         Mean_Temp_driest_quarter = bio9_23,
+         Mean_Temp_warmest_quarter = bio10_23,
+         Mean_Temp_coldest_quarter = bio11_23,
+         Annual_Precipitation = bio12_23,
+         Prec_wettest_month = bio13_23,
+         Prec_driest_month = bio14_23,
+         Prec_seasonality = bio15_23,
+         Prec_wettest_quarter = bio16_23,
+         Prec_drier_quarter = bio17_23,
+         Prec_warmest_quarter = bio18_23,
+         Prec_coldest_quarter = bio19_23)
+
+#### PARTE 4 ####
+# Exportar subset de datos solo de informacion de localidad y clusters, reordenar
+# y dar nombre unico a cada localidad (es decir combinacion de longitud y latitud unicas)
+# este nombre tendra las siglas del complejo de paramo y un numero unico.
+
+locality <- 1:483 #crear vector
+
+coordinates_clusters <- envdata_clusters %>% 
+  arrange(ParamoComplex, Department, Locality) %>%
+  mutate(complex_code = str_extract(ParamoComplex, "\\(.+\\)"), #extraer codigo
+         number_locality = locality, #adicionar numero unico
+         unique_locality = paste(complex_code, number_locality, sep = "_") #unirlos
+         ) %>% 
+  select("decimalLongitude", "decimalLatitude", "Elevation", "ParamoComplex",
+       "Department", "Locality", "clusters", "unique_locality")
+
+write_csv(coordinates_clusters, "sub_datos/coordinates_clusters.csv")
